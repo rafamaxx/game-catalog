452f3aa1b2d92fdb22abe4ecb0f98659fddc7a55
rafamaxx <rafamaxi.rm@gmail.com>
2021-05-20 16:39:23 -0300

    Add Middleware Exception

---
+++ CatalogoDeGames/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CatalogoDeGames.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch
+            {
+                await HandleExceptionAsync(context);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsJsonAsync(new{Message = "Error"});
+        }
+    }
+}
