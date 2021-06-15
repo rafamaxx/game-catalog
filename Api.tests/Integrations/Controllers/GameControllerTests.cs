8abdd129c48090b3dfe5ae5f57c683eb9b719f37
rafamaxx <rafamaxi.rm@gmail.com>
2021-06-14 22:57:07 -0300

    Tests

---
+++ Api.tests/Integrations/Controllers/GameControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using CatalogoDeGames;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using Xunit;
+
+namespace Api.tests.Integrations.Controllers
+{
+    public class GameControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpCliente;
+
+        public GameControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _httpCliente = _factory.CreateClient();
+        }
+
+        [Fact]
+        public void Obtain_GetAll_Sucess()
+        {
+           var httpClientRequest = _httpCliente.GetAsync("api/version1/Games").GetAwaiter().GetResult();
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, httpClientRequest.StatusCode);
+                   
+        }
+    }
+}
--- Api.tests/obj/Debug/net5.0/Api.tests.AssemblyInfo.cs
+++ Api.tests/obj/Debug/net5.0/Api.tests.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("CatalogoDeGames, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "D:\\Workspace\\CatalogoDeGames\\CatalogoDeGames", "CatalogoDeGames.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
